b2b07f2e1dc88cb4347c5e857be6929d6b3c07b0
KrisMT <ratiodetector@hotmail.com>
2021-12-12 12:16:16 +0700

    [MOD] Change Neovim to new version and using Lua config

--- neovim.sh
+++ neovim.sh
@@ -1,40 +1,15 @@
 #!/usr/bin/bash
 
+
+################################
+### Add neovim unstable repo
+sudo add-apt-repository ppa:neovim-ppa/unstable
+
+
 ################################
 ### Install NeoVIM && Plugins
 sudo apt install -y neovim
 
 ### Make a neovim config directory
-mkdir ~/.config/nvim
-cp ./neovim/init.vim ~/.config/nvim/init.vim
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-cp -rf ./neovim/vim-plug ~/.config/nvim
-
-cp -rf ./neovim/general ~/.config/nvim
-cp -rf ./neovim/keys ~/.config/nvim
-
-sudo apt install -y xsel
-pip3 install pynvim
-sudo apt install -y nodejs
-sudo npm i -g neovim
-
-cp -rf ./neovim/themes ~/.config/nvim
-
-sudo npm i -g yarn
-cp -rf ./neovim/plug-config ~/.config/nvim
-
-nvim +PlugInstall +qall
-nvim +PlugUpdate +qall
-
-nvim -c 'CocInstall -sync coc-json coc-python coc-snippets coc-vimlsp coc-explorer'
-cp -rf ./neovim/coc-settings.json ~/.config/nvim
-
-sudo apt install -y xorg-dev
-sudo apt install -y ranger
-pip3 install ueberzug
-cp -rf ./ranger ~/.config/ranger
-git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
-
-mkdir -p ~/.local/share/fonts
-cp -rf ./'Fira Code Regular Nerd Font Complete.otf' ~/.local/share/fonts/'Fira Code Regular Nerd Font Complete.otf' 
+mkdir ~/.config
+cp -r ./nvim ~/.config
---
+++ nvim/lua/user/plugins.lua
@@ -0,0 +1,93 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  print("Installing packer close and reopen Neovim...")
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd([[
+augroup packer_user_config
+autocmd!
+autocmd BufWritePost plugins.lua source <afile> | PackerSync
+augroup end
+]])
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init({
+  display = {
+    open_fn = function()
+      return require('packer.util').float({ border = 'single' })
+    end
+  }
+})
+
+-- returns the require for use in `config` parameter of packer's use
+-- expects the name of the config file
+function get_setup(name)
+  return string.format('require("setup/%s")', name)
+end
+
+-- Install your plugins here
+return packer.startup(function(use)
+  -- My plugins here
+  use 'wbthomason/packer.nvim' -- Have packer manage itself
+  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+
+  -- Telescope
+  use({
+    "nvim-telescope/telescope.nvim",
+    module = "telescope",
+    cmd = "Telescope",
+    requires = {
+      { "nvim-lua/popup.nvim" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    config = get_setup("telescope"),
+  })
+  use({
+    "hrsh7th/nvim-cmp",
+  })
+
+  -- LuaLine
+  use({
+    "nvim-lualine/lualine.nvim",
+    config = get_setup("lualine"),
+    requires = { "kyazdani42/nvim-web-devicons", opt = true },
+  })
+
+  --use({ "neovim/nvim-lspconfig", config = get_setup("lsp") })
+  use({
+    "numToStr/Comment.nvim",
+    opt = true,
+    keys = { "gc", "gcc" },
+    config = get_setup("comment"),
+  })
+  use({ "tpope/vim-repeat" })
+  use({ "tpope/vim-surround" })
+  use({
+    "phaazon/hop.nvim",
+    event = "BufReadPre",
+    config = get_setup("hop"),
+  })
+  --
+  -- 
+  --
+  use({ "EdenEast/nightfox.nvim", config = get_setup("nightfox") })
+  use({ "kyazdani42/nvim-web-devicons" })
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require('packer').sync()
+  end
+end)
