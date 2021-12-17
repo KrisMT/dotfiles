2948aee24315a952a4eff485663aa92888cc7817
KrisMT <ratiodetector@hotmail.com>
2021-12-17 09:25:52 +0700

    [ADD] Tab key mapping to cmp

--- nvim/lua/setup/cmp.lua
+++ nvim/lua/setup/cmp.lua
@@ -11,6 +11,7 @@ cmp.setup({
     end,
   },
   mapping = {
+    ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'}),
     ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
     ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
     ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
