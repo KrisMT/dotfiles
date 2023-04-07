7f0d04c2b5948eb0c3383de84a138b728d46bf22
KrisMT <ratiodetector@hotmail.com>
2023-04-07 13:57:05 +0700

    [MOD] Change to AstroNvim

--- neovim.sh
+++ neovim.sh
@@ -13,5 +13,14 @@ sudo apt install -y ripgrep
 
 ### Make a neovim config directory
 mkdir ~/.config
-cp -r ./nvim ~/.config
+#cp -r ./nvim ~/.config
+
+#Backup the old config
+mv ~/.config/nvim ~/.config/nvim.bak
+mv ~/.local/share/nvim ~/.local/share/nvim.bak
+mv ~/.local/state/nvim ~/.local/state/nvim.bak
+mv ~/.cache/nvim ~/.cache/nvim.bak
+
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+nvim
 
